bc6ec6aed64d0e33078169f415d69981a54e0d3d
Siarhei Prystupa <sprystupa@salesforce.com>
2021-01-14 00:47:47 -0800

    Refactoring...

--- src/main/java/com/salesforce/slack/swarm/SlackApp.java
+++ src/main/java/com/salesforce/slack/swarm/SlackApp.java
@@ -1,20 +1,23 @@
 package com.salesforce.slack.swarm;
 
 import com.google.gson.Gson;
+import com.salesforce.slack.swarm.model.Review;
+import com.salesforce.slack.swarm.model.ReviewsData;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.WebEndpoint;
 import com.slack.api.bolt.jetty.SlackAppServer;
 import com.slack.api.bolt.response.Response;
 import com.slack.api.methods.response.views.ViewsOpenResponse;
 import com.slack.api.methods.response.views.ViewsPublishResponse;
+import com.slack.api.methods.response.views.ViewsUpdateResponse;
 import com.slack.api.model.block.LayoutBlock;
+import com.slack.api.model.block.composition.OptionObject;
+import com.slack.api.model.block.composition.PlainTextObject;
 import com.slack.api.model.block.element.BlockElement;
 import com.slack.api.model.event.AppHomeOpenedEvent;
 import com.slack.api.model.view.View;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import com.salesforce.slack.swarm.model.Review;
-import com.salesforce.slack.swarm.model.ReviewsData;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -25,22 +28,43 @@ import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
+import static com.salesforce.slack.swarm.SlackApp.REVIEW_TYPE.AUTHOR;
+import static com.salesforce.slack.swarm.SlackApp.REVIEW_TYPE.PARTICIPANT;
+import static com.salesforce.slack.swarm.SlackApp.REVIEW_TYPE.UNDEFINED;
 import static com.slack.api.model.block.Blocks.*;
 import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
 import static com.slack.api.model.block.composition.BlockCompositions.plainText;
 import static com.slack.api.model.block.element.BlockElements.*;
 import static com.slack.api.model.view.Views.*;
+import static java.time.format.DateTimeFormatter.ISO_DATE;
 
 public class SlackApp {
+
+    enum REVIEW_TYPE {
+        UNDEFINED("Undefined"),
+        AUTHOR("I'm author"),
+        PARTICIPANT("I'm  participant");
+
+        private final String description;
+
+        REVIEW_TYPE(String description) {
+            this.description = description;
+        }
+
+        String getDescription() {
+            return this.description;
+        }
+    }
 
     private final static Gson GSON = new Gson();
 
     public static void main(String[] args) throws Exception {
-        // App expects env variables (SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET)
         App app = new App();
 
         app.endpoint(WebEndpoint.Method.POST, "/events", (req, ctx) -> ctx.ackWithJson(req.getRequestBodyAsString()));
@@ -54,8 +78,9 @@ public class SlackApp {
             }
             ReviewsData reviewsData = readObjectFromJsonFile("reviews.json", ReviewsData.class);
             Optional<Review> reviewOpt = reviewsData.getReviews().stream().filter(review -> param.equals(review.getId().toString())).findFirst();
-            return reviewOpt.isPresent() ?
-                    ctx.ack(asBlocks(buildCompactLayoutForReview(reviewOpt.get(), new ArrayList<>()))) : ctx.ack(":warning: Review Not Found!");
+            return reviewOpt.isPresent()
+                    ? ctx.ack(asBlocks(buildCompactLayoutForReview(reviewOpt.get(), new ArrayList<>())))
+                    : ctx.ack(":warning: Review Not Found!");
         });
 
         Pattern pattern = Pattern.compile("details_[0-9]+");
@@ -64,60 +89,82 @@ public class SlackApp {
             try {
                 reviewsData = readObjectFromJsonFile("reviews.json", ReviewsData.class);
             } catch (IOException e) {
-                return errorResponse(e.getMessage());
+                return errorResponse("Error retrieving reviews");
             }
             String actionId = req.getPayload().getActions().get(0).getActionId();
             String reviewId = actionId.substring(actionId.indexOf('_') + 1);
-            Optional<Review> reviewOpt = reviewsData.getReviews().stream().filter(review -> reviewId.equals(review.getId().toString())).findFirst();
-            Review review = reviewOpt.orElse(null);
+            Optional<Review> reviewOpt = reviewsData.getReviews().stream()
+                    .filter(review -> reviewId.equals(review.getId().toString())).findFirst();
 
             ViewsOpenResponse viewsOpenRes = ctx.client().viewsOpen(r -> r
                     .triggerId(ctx.getTriggerId())
-                    .view(buildModalView(review)));
+                    .view(buildModalView(reviewOpt.orElse(null))));
 
-            return viewsOpenRes.isOk() ? ctx.ack() : Response.builder().statusCode(500).body(viewsOpenRes.getError()).build();
+            return viewsOpenRes.isOk()
+                    ? ctx.ack()
+                    : Response.builder().statusCode(500).body(viewsOpenRes.getError()).build();
+        });
+
+        app.blockAction("change_review_type", (req, ctx) -> {
+            String selectedOption = req.getPayload().getActions().get(0).getSelectedOption().getValue();
+            REVIEW_TYPE reviewType = REVIEW_TYPE.valueOf(selectedOption);
+            ReviewsData reviewsData;
+            try {
+                switch (reviewType) {
+                    case AUTHOR: {
+                        reviewsData = readObjectFromJsonFile("reviews.json", ReviewsData.class);
+                        break;
+                    }
+                    case PARTICIPANT: {
+                        reviewsData = readObjectFromJsonFile("participant_reviews.json", ReviewsData.class);
+                        break;
+                    }
+                    default: {
+                        reviewsData = null;
+                    }
+                }
+            } catch (Exception e) {
+                return errorResponse("Error retrieving reviews");
+            }
+
+            ViewsUpdateResponse viewsUpdateResponse = ctx.client().viewsUpdate(r ->
+                    r.viewId(req.getPayload().getView().getId())
+                            .view(buildHomeView(reviewType, reviewsData)));
+
+            return viewsUpdateResponse.isOk() ? ctx.ack() : errorResponse(viewsUpdateResponse.getError());
         });
 
         app.event(AppHomeOpenedEvent.class, (payload, ctx) -> {
-
-            ReviewsData myReviewsData;
-            ReviewsData participantReviewsData;
-            try {
-                myReviewsData = readObjectFromJsonFile("reviews.json", ReviewsData.class);
-                participantReviewsData = readObjectFromJsonFile("participant_reviews.json", ReviewsData.class);
-            } catch (Exception e) {
-                return errorResponse(e.getMessage());
-            }
-
             ViewsPublishResponse viewsPublishRes = ctx.client().viewsPublish(r -> r
                     .userId(payload.getEvent().getUser())
-                    .view(buildHomeView(myReviewsData, participantReviewsData))
+                    .view(buildHomeView())
             );
 
             return viewsPublishRes.isOk() ? ctx.ack() : errorResponse(viewsPublishRes.getError());
         });
 
         SlackAppServer server = new SlackAppServer(app);
-        server.start(); // http://localhost:3000/slack/events
+        server.start();
     }
 
     private static Response errorResponse(String error) {
         return Response.builder().statusCode(500).body(error).build();
     }
 
-    private static View buildHomeView(ReviewsData reviewsData, ReviewsData participantReviewsData) {
+    private static View buildHomeView(REVIEW_TYPE reviewType, ReviewsData reviewsData) {
         List<LayoutBlock> blocks = new ArrayList<>();
-        addReviewsToBlocks("*List of pull-requests opened by you* :tada:", reviewsData, blocks);
-        blocks.add(divider());
-        blocks.add(divider());
-        blocks.add(divider());
-        addReviewsToBlocks("*List of pull-requests where you are assigned as reviewer* :tada:", participantReviewsData, blocks);
+        addReviewTypesToBlocks(reviewType, blocks);
+        addReviewsToBlocks(reviewsData, blocks);
         blocks.add(divider());
 
         return view(view -> view
                 .type("home")
                 .blocks(asBlocks(blocks.toArray(LayoutBlock[]::new)))
         );
+    }
+
+    private static View buildHomeView() {
+        return buildHomeView(UNDEFINED, null);
     }
 
     private static View buildModalView(Review review) {
@@ -132,15 +179,48 @@ public class SlackApp {
         );
     }
 
-    private static void addReviewsToBlocks(String blockName, ReviewsData reviewsData, List<LayoutBlock> blocks) {
-        String lastSeen = reviewsData != null && reviewsData.getLastSeen() != null ? reviewsData.getLastSeen().toString() : "Unknown";
-        int total = reviewsData != null && reviewsData.getTotalCount() != null ? reviewsData.getTotalCount() : 0;
-        blocks.add(section(section -> section.text(markdownText(mt -> mt.text(blockName)))));
-        if (reviewsData != null && CollectionUtils.isNotEmpty(reviewsData.getReviews())) {
-            reviewsData.getReviews().forEach(review -> buildCompactLayoutForReview(review, blocks));
+    private static void addReviewTypesToBlocks(REVIEW_TYPE reviewType, List<LayoutBlock> blocks) {
+        List<OptionObject> options = new ArrayList<>(2);
+        OptionObject option1 = OptionObject.builder()
+                .value(AUTHOR.name())
+                .text(new PlainTextObject(AUTHOR.getDescription(), false))
+                .build();
+        OptionObject option2 = OptionObject.builder()
+                .value(PARTICIPANT.name())
+                .text(new PlainTextObject(PARTICIPANT.getDescription(), false))
+                .build();
+        options.add(option1);
+        options.add(option2);
+        OptionObject initialOption;
+        switch (reviewType) {
+            case AUTHOR:
+                initialOption = option1;
+                break;
+            case PARTICIPANT:
+                initialOption = option2;
+                break;
+            default:
+                initialOption = null;
         }
+        blocks.add(section(section -> section
+                        .text(markdownText(mt -> mt.text("*Review request type:*")))
+                        .accessory(staticSelect(staticSelect -> staticSelect
+                                .actionId("change_review_type").options(options).initialOption(initialOption)))
+                )
+        );
+    }
+
+    private static void addReviewsToBlocks(ReviewsData reviewsData, List<LayoutBlock> blocks) {
+        if (reviewsData == null || CollectionUtils.isEmpty(reviewsData.getReviews())) return;
+
+        String lastSeen = reviewsData.getLastSeen() != null ? reviewsData.getLastSeen().toString() : "Unknown";
+        int total = reviewsData.getTotalCount() != null ? reviewsData.getTotalCount() : 0;
+        blocks.add(section(section -> section.text(markdownText(mt -> mt.text("*Review requests*")))));
+        reviewsData.getReviews().forEach(review -> buildCompactLayoutForReview(review, blocks));
         blocks.add(divider());
-        blocks.add(section(section -> section.text(markdownText(mt -> mt.text("Last seen: " + lastSeen + "\tTotal reviews: " + total)))));
+        blocks.add(section(section -> section.text(
+                markdownText(mt -> mt.text("Last seen: " + lastSeen + "\tTotal reviews: " + total))))
+        );
     }
 
     private static LayoutBlock[] buildCompactLayoutForReview(Review review, List<LayoutBlock> blocks) {
@@ -158,15 +238,20 @@ public class SlackApp {
         LocalDate date = Instant.ofEpochMilli(review.getCreated()).atZone(ZoneId.systemDefault()).toLocalDate();
         blocks.add(context(context -> context
                 .elements(asContextElements(
-                        imageElement(image -> image.imageUrl("https://api.slack.com/img/blocks/bkb_template_images/task-icon.png").altText("Not Found")),
-                        markdownText("Submitted by: *" + review.getAuthor() + "* on " + date.format(DateTimeFormatter.ISO_DATE))
+                        imageElement(image ->
+                                image.imageUrl("https://api.slack.com/img/blocks/bkb_template_images/task-icon.png")
+                                        .altText("Image not found")
+                        ),
+                        markdownText("Submitted by: *" + review.getAuthor() + "* on " + date.format(ISO_DATE))
                 ))));
 
         List<BlockElement> actionsList = new ArrayList<>();
         actionsList.add(button(b -> b.text(plainText(pt -> pt.text("View Details"))).value("details_" + review.getId()).actionId("details_" + review.getId())));
         if (review.getState().startsWith("needs")) {
-            actionsList.add(button(b -> b.text(plainText(pt -> pt.text("Approve"))).style("primary").value("approve_" + review.getId()).actionId("approve_" + review.getId())));
-            actionsList.add(button(b -> b.text(plainText(pt -> pt.text("Decline"))).style("danger").value("decline_" + review.getId()).actionId("decline_" + review.getId())));
+            actionsList.add(button(b -> b.text(plainText(pt -> pt.text("Approve")))
+                    .style("primary").value("approve_" + review.getId()).actionId("approve_" + review.getId())));
+            actionsList.add(button(b -> b.text(plainText(pt -> pt.text("Decline")))
+                    .style("danger").value("decline_" + review.getId()).actionId("decline_" + review.getId())));
         }
         blocks.add(actions(actions -> actions.elements(asElements(actionsList.toArray(BlockElement[]::new)))));
         return blocks.toArray(LayoutBlock[]::new);
@@ -174,9 +259,9 @@ public class SlackApp {
 
     private static LayoutBlock[] buildLayoutForReview(Review review) {
         if (review == null) return new LayoutBlock[0];
-        List<LayoutBlock> blocks = new ArrayList<>();
-        blocks.add(section(section -> section.text(markdownText(mt -> mt.text(getReviewDescription(review))))));
-        return blocks.toArray(LayoutBlock[]::new);
+        return new LayoutBlock[] {
+                section(section -> section.text(markdownText(mt -> mt.text(getReviewDescription(review)))))
+        };
     }
 
     private static String getReviewDescription(Review review) {
